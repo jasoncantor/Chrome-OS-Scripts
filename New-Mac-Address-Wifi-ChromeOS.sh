64c34bd9c266f32ab2fc1137a385e7abc55e6bd9
Jason Cantor <jason.cantor@stewardschool.org>
2019-11-19 12:19:00 -0500

    Update New-Mac-Address-Wifi-ChromeOS.sh

--- New-Mac-Address-Wifi-ChromeOS.sh
+++ New-Mac-Address-Wifi-ChromeOS.sh
@@ -1,3 +1,5 @@
-sudo ifconfig wlan0 down
-echo fa:55:3d:$[RANDOM%10]$[RANDOM%10]:$[RANDOM%10]$[RANDOM%10]:$[RANDOM%10]$[RANDOM%10] | sudo ifconfig wlan0 hw ether tee
-sudo ifconfig wlan0 up
+#sudo ifconfig wlan0 down
+#sudo ifconfig wlan0 hw ether echo fa:55:3d:$[RANDOM%10]$[RANDOM%10]:$[RANDOM%10]$[RANDOM%10]:$[RANDOM%10]$[RANDOM%10]
+#sudo ifconfig wlan0 up
+current_date=$(date)
+echo "Today is $current_date"
