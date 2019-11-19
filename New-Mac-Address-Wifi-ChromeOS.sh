74b95c721a3dafa4994c76b223bb13e14b8fc480
Jason Cantor <jason.cantor@stewardschool.org>
2019-11-19 12:36:48 -0500

    Update New-Mac-Address-Wifi-ChromeOS.sh

--- New-Mac-Address-Wifi-ChromeOS.sh
+++ New-Mac-Address-Wifi-ChromeOS.sh
@@ -1,7 +1,7 @@
 hexchars="0123456789ABCDEF"
 end=$( for i in {1..6} ; do echo -n ${hexchars:$(( $RANDOM % 16 )):1} ; done | sed -e 's/\(..\)/-\1/g' )
-echo fa-55-3d$end
- 
+MAC=fa-55-3d$end
+
 ifconfig wlan0 down
 ifconfig wlan0 hw ether $MAC
 ifconfig wlan0 up
