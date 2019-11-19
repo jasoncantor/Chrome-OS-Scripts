40b304b40ceb7adf4f4aa54e2e07e3b8636443e6
Jason Cantor <jason.cantor@stewardschool.org>
2019-11-19 12:26:04 -0500

    Update New-Mac-Address-Wifi-ChromeOS.sh

--- New-Mac-Address-Wifi-ChromeOS.sh
+++ New-Mac-Address-Wifi-ChromeOS.sh
@@ -1,5 +1,5 @@
 sudo ifconfig wlan0 down
-mac_address='echo fa:55:3d:$[RANDOM%10]$[RANDOM%10]:$[RANDOM%10]$[RANDOM%10]:$[RANDOM%10]$[RANDOM%10]'
+mac_address=(echo fa:55:3d:$[RANDOM%10]$[RANDOM%10]:$[RANDOM%10]$[RANDOM%10]:$[RANDOM%10]$[RANDOM%10])
 sudo ifconfig wlan0 hw ether $mac_address
 sudo ifconfig wlan0 up
 echo "Changed"
