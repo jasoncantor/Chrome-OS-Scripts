20b0965aa254e7b328e7890417ac154065b11389
Jason Cantor <jason.cantor@stewardschool.org>
2020-05-11 11:12:54 -0400

    Create lamp.sh

---
+++ Linux Scripts/lamp.sh
@@ -0,0 +1,12 @@
+sudo apt-get update
+sudo apt-get install curl
+sudo service apache2 restart
+
+sudo apt-get install php-curl
+sudo service apache2 restart
+
+sudo a2enmod rewrite
+sudo service apache2 restart
+
+sudo apt-get install unzip
+sudo service apache2 restart
