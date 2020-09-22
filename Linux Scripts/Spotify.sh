f5f6cca40e004c552b2e1319d804bead61cd37b9
Jason Cantor <jason.cantor@stewardschool.org>
2020-09-22 10:42:45 -0400

    Update Spotify.sh

--- Linux Scripts/Spotify.sh
+++ Linux Scripts/Spotify.sh
@@ -1,4 +1,4 @@
 curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
 curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-apt-get update && apt-get install spotify-client
+apt-get update && apt-get install spotify-client -y
